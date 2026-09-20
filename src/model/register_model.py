8e7bbf86aacbef5c53324a97214237fe9635a590
swati-mishra07 <Aanamishra2407@gmail.com>
2026-09-20 17:16:48 +0530

    CICD run-10

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -14,7 +14,7 @@ warnings.filterwarnings("ignore")
 
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
-Set up DagsHub credentials for MLflow tracking
+# Set up DagsHub credentials for MLflow tracking
 dagshub_token = os.getenv("CAPSTONE_TEST")
 if not dagshub_token:
     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
